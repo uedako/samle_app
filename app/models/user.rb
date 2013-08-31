f00c751bc68371bcc5c9415a61c413d010b47c62
Kouji ueda <ueda@neops-dev1.(none)>
2013-08-31 22:41:21 +0900

    6 show end

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
-  before_save { self.email = email.downcase } 
+  before_save { email.downcase! } 
 end
